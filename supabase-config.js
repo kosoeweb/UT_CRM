06937cab5ca387bec90d0f51aa5b1fa1683fbf1b
kosoeweb <kosoe.web@gmail.com>
2026-04-14 15:11:32 +0630

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// သင့်ရဲ့ Supabase Project ထဲက တကယ့် URL နဲ့ Key ကို အစားထိုးထည့်ပါ
+const SUPABASE_URL = 'https://zdifzvpjmmmsfitjsraw.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_Eghy_AeArZhWU86ETOoWqg_FI8xXh9P';
+
+// ⬇️ ဒီစာကြောင်းက အရေးကြီးဆုံးပါ (Client ဖန်တီးတဲ့ အဆင့်) ⬇️
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
